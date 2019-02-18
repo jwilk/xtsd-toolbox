f2babc8aa91792aa28d087ad35c70c48c63c44cd
Jakub Wilk <jwilk@jwilk.net>
2019-02-18 08:35:31 +0100

    libxtsd: add.
    Move color fixing code here to libxtsd.

---
+++ libxtsd.py
@@ -0,0 +1,23 @@
+# Copyright © 2019 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+'''
+xterm screendump helper functions
+'''
+
+import re
+
+def fix_color(s):
+    match = re.match(r'^rgb[(](\d+[.]\d+)%, (\d+[.]\d+)%, (\d+[.]\d+)%[)]$', s)
+    result = ['#']
+    for i in range(1, 4):
+        v = match.group(i)
+        v = float(v)
+        v = int(15 * v / 100.0)
+        v = '{0:x}'.format(v)
+        result += [v]
+    return ''.join(result)
+
+__all__ = ['fix_color']
+
+# vim:ts=4 sts=4 sw=4 et
